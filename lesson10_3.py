d3b5744e70aae8ed60a85e3e95075fd6d299defe
Han7502 <150802061+Han7502@users.noreply.github.com>
2024-03-22 04:59:44 +0000

    modify

--- lesson10_3.py
+++ lesson10_3.py
@@ -29,5 +29,7 @@ def getStudents(student_nums:int=1, scores_nums:int=2) -> list[list]:
 if __name__ == '__main__':
     s_nums = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
     o_nums:int = pyip.inputInt("請輸入科目數(1~7):",min=1,max=7)
-    students:list[list] = getStudents(student_nums=s_nums,scores_nums=o_nums)
+    students:list[list] = getStudents(stud
+                                      
+                                      ent_nums=s_nums,scores_nums=o_nums)
     print(students)
